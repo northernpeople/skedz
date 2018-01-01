09285b0f31fa31d5a8b34a3ad46d59ca6c9b0db9
Stepan Razdobarov <northernpeople@gmail.com>
2018-01-01 12:26:42 -0600

    wip

--- src/main/java/online/skedz/scheduler/core/user/UserRepository.java
+++ src/main/java/online/skedz/scheduler/core/user/UserRepository.java
@@ -4,10 +4,9 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
 	public User findByUsername(String userName);
